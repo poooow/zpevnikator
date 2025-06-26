8005e966a6711372d4535b3da0d9157c1bd06c9b
pow <poooow@gmail.com>
2025-06-27 00:50:34 +0200

    App icons

--- vite.config.ts
+++ vite.config.ts
@@ -36,14 +36,35 @@ export default defineConfig({
         short_name: 'Zpěvníkátor',
         description: 'Zpěvníkátor',
         theme_color: '#ffffff',
+        background_color: '#ffffff',
         start_url: '/',
         display: 'standalone',
+        scope: '/',
         icons: [
           {
-            src: '/icon-192x192.png',
+            src: 'icon-192x192.png',
             sizes: '192x192',
-            type: 'image/png'
+            type: 'image/png',
+            purpose: 'any'
           },
+          {
+            src: 'icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+          {
+            src: 'maskable-icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'maskable any'
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable any'
+          }
         ]
       },
       workbox: {
