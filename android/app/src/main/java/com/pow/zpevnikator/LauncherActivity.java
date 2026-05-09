96660d80e0acb5c82d92be1f79459b4f9c21de79
pow <poooow@gmail.com>
2026-05-09 16:52:26 +0200

    rename app

--- android/app/src/main/java/com/pow/zpevnikator/LauncherActivity.java
+++ android/app/src/main/java/com/pow/zpevnikator/LauncherActivity.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package cz.samius.zpevnik;
+package com.pow.zpevnikator;
 
 import android.content.pm.ActivityInfo;
 import android.net.Uri;
